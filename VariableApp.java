781164a77177b30ad71e58071e251a3acb9a19c4
egoing <egoing@gmail.com>
2019-09-23 21:28:29 +0900

    변수

---
+++ VariableApp.java
@@ -0,0 +1,16 @@
+
+public class VariableApp {
+
+	public static void main(String[] args) {
+		
+		int a = 1;
+		String b = "Hello World";
+		
+		double c = 1;
+		System.out.println(c);
+		int d = (int)1.8;
+		System.out.println(d);
+		
+	}
+
+}
