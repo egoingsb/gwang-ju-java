571d864f2e8b16434ba4ecabe3dfe25e37aa7afb
egoing <egoing@gmail.com>
2019-09-23 21:28:38 +0900

    부가세앱

---
+++ VAT.java
@@ -0,0 +1,26 @@
+
+public class VAT {
+
+	public static void main(String[] args) {
+		double total = 1234;
+		double VAT = (total*0.1);
+		double supplyValue = total - VAT;
+		
+		double person1 = supplyValue*0.5;
+		double person2 = supplyValue*0.3;
+		double person3 = supplyValue*0.2;
+		
+		System.out.println("합계:"+total);
+		System.out.println("부가가치세:"+VAT);
+		System.out.println("공급가액:"+supplyValue);
+		
+		System.out.println("동업자1:"+person1);
+		System.out.println("동업자2:"+person2);
+		System.out.println("동업자3:"+person3);
+		
+		//Bank.send("동업자1의 계좌번호", person1);
+		//Bank.send("동업자2의 계좌번호", person2);
+		//Bank.send("동업자3의 계좌번호", person2);
+	}
+
+}
