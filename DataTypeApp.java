09cd3493a817201898e369c9887d0e3b57cec47b
egoing <egoing@gmail.com>
2019-09-23 21:28:21 +0900

    Datatype

---
+++ DataTypeApp.java
@@ -0,0 +1,18 @@
+
+public class DataTypeApp {
+
+	public static void main(String[] args) {
+		System.out.println(1); // integer
+		System.out.println("Hello World!!"); // String
+		System.out.println("1");
+		System.out.println(1+1); //2
+		System.out.println("1"+"1"); 
+		System.out.println(1.8); // double
+		System.out.println(Math.floor(1.8));
+		System.out.println(Math.ceil(1.8));
+		System.out.println("Hello World".length());
+		System.out.println("Hello World".contains("hjfhjkf"));
+		
+	}
+
+}
