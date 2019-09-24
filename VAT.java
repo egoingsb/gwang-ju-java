b5f25c6fad8dd646ea612242fb22fbeeb99835a1
egoing <egoing@gmail.com>
2019-09-24 22:19:33 +0900

    부가세 앱에 입력값 추가

--- VAT.java
+++ VAT.java
@@ -10,17 +10,14 @@ public class VAT {
 		double person2 = supplyValue*0.3;
 		double person3 = supplyValue*0.2;
 		
-		System.out.println("합계:"+total);
-		System.out.println("부가가치세:"+VAT);
-		System.out.println("공급가액:"+supplyValue);
+		System.out.println("total:"+total);
+		System.out.println("VAT:"+VAT);
+		System.out.println("supply value:"+supplyValue);
 		
-		System.out.println("동업자1:"+person1);
-		System.out.println("동업자2:"+person2);
-		System.out.println("동업자3:"+person3);
+		System.out.println("person 1:"+person1);
+		System.out.println("person 2:"+person2);
+		System.out.println("person 3:"+person3);
 		
-		//Bank.send("동업자1의 계좌번호", person1);
-		//Bank.send("동업자2의 계좌번호", person2);
-		//Bank.send("동업자3의 계좌번호", person2);
 	}
 
 }
