7f3a3828e77464693d77aeee8e116ec4e2b4a121
egoing <egoing@gmail.com>
2019-09-24 22:20:16 +0900

    메소드 기능용 샘플

---
+++ MyMethodApp.java
@@ -0,0 +1,17 @@
+
+public class MyMethodApp {
+
+	public static void main(String[] args) {
+		//..
+		print("A");
+		print("B");
+		print("C");
+
+	}
+
+	public static void print(String text) {
+		System.out.println(text);
+		System.out.println(text);
+	}
+
+}
