1d2935d3d664eb7bc74e63f31d3b8541b181f330
egoing <egoing@gmail.com>
2019-09-25 15:44:51 +0900

    부가세에 값으로서 메소드 적용

--- VAT.java
+++ VAT.java
@@ -3,21 +3,27 @@ public class VAT {
 
 	public static void main(String[] args) {
 		double total =  Double.parseDouble(args[0]);
-		double VAT = (total*0.1);
-		double supplyValue = total - VAT;
+		double ratio = 0.1;
+		double VAT = taxVAT(total, ratio);
+		double supplyValue = taxSupplyValue(total, VAT);
+		taxPrint(total, VAT, supplyValue);
 		
-		double person1 = supplyValue*0.5;
-		double person2 = supplyValue*0.3;
-		double person3 = supplyValue*0.2;
-		
+	}
+
+	public static void taxPrint(double total, double VAT, double supplyValue) {
 		System.out.println("total:"+total);
 		System.out.println("VAT:"+VAT);
 		System.out.println("supply value:"+supplyValue);
-		
-		System.out.println("person 1:"+person1);
-		System.out.println("person 2:"+person2);
-		System.out.println("person 3:"+person3);
-		
+	}
+
+	public static double taxSupplyValue(double total, double VAT) {
+		double supplyValue = total - VAT;
+		return supplyValue;
+	}
+
+	public static double taxVAT(double total, double ratio) {
+		double VAT = (total*ratio);
+		return VAT;
 	}
 
 }
