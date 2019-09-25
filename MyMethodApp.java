e60705008b3cf903169670f1a4dbf4126f507f6c
egoing <egoing@gmail.com>
2019-09-25 15:44:05 +0900

    값으로서 메소드화

--- MyMethodApp.java
+++ MyMethodApp.java
@@ -1,19 +1,24 @@
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class MyMethodApp {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		//..
-		printA();
+		System.out.println(twoTimes("---","A"));
 		//..
-		printA();
-		//..
-		printA();
+		FileWriter file1 = new FileWriter("result.txt");
+		file1.write(twoTimes("***", "B"));
+		file1.close();
 
 	}
 
-	private static void printA() {
-		System.out.println("A");
-		System.out.println("A");
+	private static String twoTimes(String delimiter, String text) {
+		return delimiter + "\n" + text + "\n" + text;
 	}
+	private static void printVoice(String delimiter, String text) {
+		
+	}
+	
 
 }
