349ea32ef8bb47bc03ef673d07ec04d7322c6daf
egoing <egoing@gmail.com>
2019-09-26 16:40:01 +0900

    배열의 기본 형식

---
+++ ArrayApp.java
@@ -0,0 +1,11 @@
+
+public class ArrayApp {
+
+	public static void main(String[] args) {
+		String[] member = new String[3];
+		member[0] = "egoing";
+		member[1] = "duru";
+		System.out.println(member.length);
+	}
+
+}
