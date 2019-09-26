e6fb20eaf13715d45a6df054ec32554fcb4cde84
egoing <egoing@gmail.com>
2019-09-26 15:39:35 +0900

    비교연산자 예제

---
+++ ConditionalApp.java
@@ -0,0 +1,16 @@
+
+public class ConditionalApp {
+
+	public static void main(String[] args) {
+		
+		System.out.println("A");
+		if(false) {
+			System.out.println("B1");
+		} else {
+			System.out.println("B2");
+		}
+		System.out.println("C");
+		
+	}
+
+}
