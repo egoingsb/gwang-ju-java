5f9d132820900068fb808065007a78b4805aa6ee
egoing <egoing@gmail.com>
2019-09-26 17:01:05 +0900

    배열 + 반복문

--- ArrayApp.java
+++ ArrayApp.java
@@ -2,10 +2,16 @@
 public class ArrayApp {
 
 	public static void main(String[] args) {
-		String[] member = new String[3];
+		String[] member = new String[4];
 		member[0] = "egoing";
 		member[1] = "duru";
+		member[2] = "leezche";
+		member[3] = "taeho";
 		System.out.println(member.length);
+		
+		for(int i=0; i<member.length; i++) {
+			System.out.println(member[i]+"님 ^^");
+		}
 	}
 
 }
