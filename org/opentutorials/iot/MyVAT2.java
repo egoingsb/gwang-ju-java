bc3a7280673fd72ed98a0d41ba3f4a9bcf016196
egoing <egoing@gmail.com>
2019-09-26 14:44:14 +0900

    나의 VAT 앱 - 강사버전

---
+++ org/opentutorials/iot/MyVAT2.java
@@ -0,0 +1,22 @@
+package org.opentutorials.iot;
+
+import javax.swing.JOptionPane;
+
+public class MyVAT2 {
+
+	public static void main(String[] args) {
+		String inputTotal = JOptionPane.showInputDialog("Please input total : ");
+		double total = Double.parseDouble(inputTotal);
+		double VAT = 10000.0 * 0.1;
+		double supplyValue = total - VAT;
+		
+		System.out.println("Total : " + total);
+		System.out.println("VAT : " + VAT);
+		System.out.println("Supply Value : " + supplyValue);
+		
+		System.out.println("Person1 : " + supplyValue * 0.5);
+		System.out.println("Person2 : " + supplyValue * 0.3);
+		System.out.println("Person3 : " + supplyValue * 0.2);
+	}
+
+}
