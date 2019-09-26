224822b5efb893f7a56e40aa8f2effbff9d79298
egoing <egoing@gmail.com>
2019-09-26 15:15:37 +0900

    Boolean & 비교연산자

---
+++ BooleanApp.java
@@ -0,0 +1,19 @@
+
+public class BooleanApp {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World".contains("hjgfghjfj"));
+		System.out.println(true);
+		System.out.println(false);
+		//String true = "hi";
+		System.out.println(true && true); // true
+		System.out.println(true && false); // false
+		
+		System.out.println(1>2); //false
+		System.out.println(1<2); //true
+		System.out.println(1==2); // false
+
+	}
+
+}
